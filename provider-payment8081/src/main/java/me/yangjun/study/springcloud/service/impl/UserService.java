1c1e5067c68204c9b80c872b8c1dbcee12cfceaf
brady <mooejun@163.com>
2023-01-11 17:20:35 +0800

    :wrench: 修改配置文件

--- provider-payment8081/src/main/java/me/yangjun/study/springcloud/service/impl/UserService.java
+++ provider-payment8081/src/main/java/me/yangjun/study/springcloud/service/impl/UserService.java
@@ -1,15 +1,15 @@
 package me.yangjun.study.springcloud.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import me.yangjun.study.springcloud.common.entitys.User;
 import me.yangjun.study.springcloud.dao.UserMapper;
 import me.yangjun.study.springcloud.service.IUserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService implements IUserService {
-    @Autowired
-    private UserMapper userMapper;
+    private final UserMapper userMapper;
 
     @Override
     public User getById(Long id) {

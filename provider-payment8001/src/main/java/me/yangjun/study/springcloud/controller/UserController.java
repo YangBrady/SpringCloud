6800797f60d64144f6aa83f456142b5b36e8aa1d
YangBrady <mooejun@163.com>
2025-08-05 21:32:06 +0800

    :sparkles: DiscoveryClient服务发现

--- provider-payment8001/src/main/java/me/yangjun/study/springcloud/controller/UserController.java
+++ provider-payment8001/src/main/java/me/yangjun/study/springcloud/controller/UserController.java
@@ -1,9 +1,13 @@
 package me.yangjun.study.springcloud.controller;
 
+import java.util.List;
+import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import me.yangjun.study.springcloud.common.entitys.CommonResult;
 import me.yangjun.study.springcloud.common.entitys.User;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +20,9 @@ public class UserController {
     @Value("${server.port}")
     private String serverPort;
 
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/findById/{id}")
     public CommonResult<User> findById(@PathVariable("id") Long id) {
         User user = new User();
@@ -23,5 +30,21 @@ public class UserController {
         user.setName("yangjun");
         user.setAge(18);
         return new CommonResult<>(200, "查询成功, serverPort=" + serverPort, user);
+    }
+
+    @GetMapping("/discover")
+    public void discover() {
+        List<String> serviceList = discoveryClient.getServices();
+
+        // 获取所有微服务
+        for (String service : serviceList) {
+            log.info("service: {}", service);
+        }
+
+        // 获取所有 PAYMENT-SERVICE 服务实例
+        List<ServiceInstance> instanceList = discoveryClient.getInstances("PAYMENT-SERVICE");
+        for (ServiceInstance serviceInstance : instanceList) {
+            log.info("PAYMENT-SERVICE instance: {}", serviceInstance.getInstanceId());
+        }
     }
 }

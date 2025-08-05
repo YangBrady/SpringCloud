0dda8c3e886e7436db13a576b0166300fe5e93ae
YangBrady <mooejun@163.com>
2025-08-05 21:24:14 +0800

    :coffin: 删除没用的依赖

--- provider-payment8001/src/main/java/me/yangjun/study/springcloud/Application8001.java
+++ provider-payment8001/src/main/java/me/yangjun/study/springcloud/Application8001.java
@@ -1,6 +1,5 @@
 package me.yangjun.study.springcloud;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 

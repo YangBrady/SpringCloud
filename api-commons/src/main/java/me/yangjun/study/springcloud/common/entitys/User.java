f27d907a17cf08cd1bd0de76ecd8ce2a290bb4c2
YangBrady <mooejun@163.com>
2025-08-13 17:33:09 +0800

    :sparkles: 增加备注

--- api-commons/src/main/java/me/yangjun/study/springcloud/common/entitys/User.java
+++ api-commons/src/main/java/me/yangjun/study/springcloud/common/entitys/User.java
@@ -2,15 +2,16 @@ package me.yangjun.study.springcloud.common.entitys;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class User implements Serializable {
     private static final long serialVersionUID = -6903247912037987388L;
     @TableId(value = "id", type = IdType.INPUT)
@@ -18,4 +19,5 @@ public class User implements Serializable {
     private String name;
     private Integer age;
     private String email;
+    private String remark;
 }

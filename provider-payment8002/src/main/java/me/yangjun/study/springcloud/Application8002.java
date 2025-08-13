4f0d8881434eb5e622687fb236f5e86d86550303
YangBrady <mooejun@163.com>
2025-08-13 16:15:45 +0800

    :sparkles: Hystrix基本使用

--- provider-payment8002/src/main/java/me/yangjun/study/springcloud/Application8002.java
+++ provider-payment8002/src/main/java/me/yangjun/study/springcloud/Application8002.java
@@ -1,13 +1,11 @@
 package me.yangjun.study.springcloud;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableHystrix
-@MapperScan("me.yangjun.study.springcloud.dao")
 public class Application8002 {
     public static void main(String[] args) {
         SpringApplication.run(Application8002.class, args);
--- provider-payment8002/src/main/java/me/yangjun/study/springcloud/service/impl/UserService.java
+++ provider-payment8002/src/main/java/me/yangjun/study/springcloud/service/impl/UserService.java
@@ -1,28 +1,22 @@
 package me.yangjun.study.springcloud.service.impl;
 
-import java.util.concurrent.TimeUnit;
-
-import org.springframework.stereotype.Service;
-
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
-
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.yangjun.study.springcloud.common.entitys.User;
-import me.yangjun.study.springcloud.dao.UserMapper;
 import me.yangjun.study.springcloud.service.IUserService;
+import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService implements IUserService {
-    private final UserMapper userMapper;
-
     @Override
     public User getById(Long id) {
         log.debug("id={}", id);
-        return userMapper.selectById(id);
+        return new User(id, "yangjun", 18, "123@163.com");
     }
 
     @Override
@@ -34,20 +28,20 @@ public class UserService implements IUserService {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        return userMapper.selectById(id);
+        return new User(id, "yangjun", 18, "123@163.com");
     }
 
     @Override
-    @HystrixCommand(fallbackMethod = "findByIdErrorHandler", commandProperties = {
-        @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")})
-    public User findByIdTimeOutHystrix(Long id) {
-        try {
-            log.debug("id={}", id);
-            TimeUnit.SECONDS.sleep(5);
-            log.debug("sleep over"); // 熔断以后不会执行
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+    @HystrixCommand(
+        fallbackMethod = "findByIdTimeOutHystrixHandler",
+        commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
         }
+    )
+    public User findByIdTimeOutHystrix(Long id) {
+        log.debug("id={}", id);
+        try {TimeUnit.SECONDS.sleep(5);} catch (InterruptedException e) {throw new RuntimeException(e);}
+        log.debug("sleep over"); // 熔断以后不会执行
         return null;
     }
 
@@ -57,8 +51,8 @@ public class UserService implements IUserService {
      * @param id id
      * @return {@link User}
      */
-    private User findByIdErrorHandler(Long id) {
-        log.error("服务端熔断, id={}, ", id);
+    private User findByIdTimeOutHystrixHandler(Long id) {
+        log.error("服务端降级, id={}, ", id);
         return new User();
     }
 }
